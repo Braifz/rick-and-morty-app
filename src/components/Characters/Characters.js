57f8746e9f2bb481572d849c78c86826d25daacb
Braifz <braian94lp@gmail.com>
2022-05-08 02:38:00 -0300

    create component characters

---
+++ src/components/Characters/Characters.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import { Row } from "react-bootstrap";
+import Character from "../Character/Character";
+
+const Characters = ({ characters }) => {
+  return (
+    <Row className="justify-content-center">
+      {characters?.length > 0 &&
+        characters.map((character) => <Character {...character} />)}
+    </Row>
+  );
+};
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      created: PropTypes.string,
+      gender: PropTypes.string,
+      id: PropTypes.number,
+      name: PropTypes.string,
+      species: PropTypes.string,
+      status: PropTypes.string,
+    }).isRequired
+  ),
+};
+
+export default Characters;
