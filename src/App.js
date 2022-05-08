260fe3c7cb535268fccf07c4a002bf4746131868
Braifz <braian94lp@gmail.com>
2022-05-08 02:30:37 -0300

    add state, search and characters component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Characters from "./components/Characters/Characters";
+import "./App.scss";
+import { useFetch } from "./hooks/useFetch";
+import { Container } from "react-bootstrap";
+import Loading from "./components/common/Loading";
+import Search from "./components/Search/Search";
 
 function App() {
+  const [endpoint, setEndpoint] = useState("character");
+  const { data, loading, error } = useFetch(endpoint);
+  const { results: characters, info } = data;
+
+  const findCharacters = (character) => {
+    setEndpoint(`character?name=${character}`);
+  };
+
+  if (loading) return <Loading />;
+  if (error) return <h3>Ocurrio un error</h3>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Search findCharacters={findCharacters} />
+      <Characters characters={characters} />
+    </Container>
   );
 }
 
